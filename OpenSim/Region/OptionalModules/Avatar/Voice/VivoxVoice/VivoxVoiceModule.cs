743e7606aaa2534d884b72f8304f031dfd7c1ac3
Dirk Husemann <hud@zurich.ibm.com>
2009-02-24 11:55:39 +0100

    well...not quite, mr husemann...fixing fix

--- OpenSim/Region/OptionalModules/Avatar/Voice/VivoxVoice/VivoxVoiceModule.cs
+++ OpenSim/Region/OptionalModules/Avatar/Voice/VivoxVoice/VivoxVoiceModule.cs
@@ -253,7 +253,7 @@ namespace OpenSim.Region.OptionalModules.Avatar.Voice.VivoxVoice
                             case "403" : // Account does not exist
                             case "ERR":
                                 resp = VivoxCreateAccount(agentname,password);
-                                if (XmlFind(resp, "response.level0.body.code", out code))
+                                if (XmlFind(resp, "response.level0.status", out code))
                                 {
                                     switch (code)
                                     {
